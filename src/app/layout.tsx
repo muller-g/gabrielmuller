049dec6fb7d7f42df575a03613d4bac2467f99d5
muller-g <gabriel_muller98@hotmail.com>
2024-05-17 22:28:23 -0300

    chroe: keep single page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/ui/header";
-import Footer from "@/ui/footer";
 
 export const metadata: Metadata = {
   title: "Gabriel - Full Stack",
@@ -16,9 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 import "./index.css";
-import HomeSection from "@/_sections/home";
-import AboutSection from "@/_sections/about";
-import ServicesSection from "@/_sections/services";
-import PortfolioSection from "@/_sections/portfolio";
-import ResumeSection from "@/_sections/resume";
-import SocialSection from "@/_sections/social";
-import ContactSection from "@/_sections/contact";
+import me from "../assets/eu.jpeg";
+import LinksButton from "@/components/links-button";
+import wpp from "../assets/wpp.jpg";
+import linkedin from "../assets/linkedin.png";
+import github from "../assets/github.png";
 
-export default function Home() {
-  return (
-    <main className="home-content">
-        <HomeSection />
-        <AboutSection />
-        <ServicesSection />
-        <PortfolioSection />
-        <ResumeSection />
-        {/*<SocialSection />*/}
-        <ContactSection />
-    </main>
-  );
+export default function MeusLinks() {
+    return (
+        <main className="my-links">
+            <div className="my-links-container">
+                <div className="title-img" style={{backgroundImage: `url("${me.src}")`}}></div>
+                <h1 className="instagram-title">@_muller.dev</h1>
+                <p className="description">Vamos colocar seu projeto na internet e alcançar seus objetivos!</p>
+                <div className="redirect-links">
+                    <LinksButton text={'Fale comigo'} url={'https://api.whatsapp.com/send?phone=5547997003649&text=Ol%C3%A1%2C%20venho%20atrav%C3%A9s%20do%20site%20%F0%9F%98%80'} icon={wpp} />
+                    <LinksButton text={'Linkedin'} url={'https://www.linkedin.com/in/muller-g/'} icon={linkedin} />
+                    <LinksButton text={'GitHub'} url={'https://github.com/muller-g'} icon={github} />
+                </div>
+            </div>
+        </main>
+    );
 }
