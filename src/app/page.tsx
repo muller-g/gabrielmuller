b4caa7072a12741b26f2806e062266b1c38293e5
muller-g <gabriel_muller98@hotmail.com>
2024-05-20 14:19:10 -0300

    feat: description change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,9 @@ export default function MeusLinks() {
             <div className="my-links-container">
                 <div className="title-img" style={{backgroundImage: `url("${me.src}")`}}></div>
                 <h1 className="instagram-title">@_muller.dev</h1>
-                <p className="description">Vamos colocar seu projeto na internet e alcançar seus objetivos!</p>
+                <p className="description">👨‍💻 Atuando desde 2019</p>
+                <p className="description">❤️ Apaixonado por tecnologia</p>
+                <p className="description">#OpenToWork</p>
                 <div className="redirect-links">
                     <LinksButton text={'Fale comigo'} url={'https://api.whatsapp.com/send?phone=5547997003649&text=Ol%C3%A1%2C%20venho%20atrav%C3%A9s%20do%20site%20%F0%9F%98%80'} icon={wpp} />
                     <LinksButton text={'Linkedin'} url={'https://www.linkedin.com/in/muller-g/'} icon={linkedin} />
